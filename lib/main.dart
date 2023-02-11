0a10398c2c5c483925b5b95bad4033a2c2b3eb92
Bilail <bilail.benyahia@outlook.fr>
2023-02-11 15:08:10 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(MyApp())
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key? key}) : super(key : key);
+
+}
+
---
+++ lib/reader/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('Tuto GDSC')
+      ),
+      body : Column(
+        children: [
+          ListView()
+        ]
+
+      ),
+    );
+  }
+}
