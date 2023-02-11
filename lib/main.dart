da602f7ef864cf3f012fa1a32cc09760996ac4bc
Bilail <bilail.benyahia@outlook.fr>
2023-02-11 15:09:48 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:gdsc_tuto/reader/screens/home.dart';
 
 void main(){
-  runApp(MyApp())
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({key? key}) : super(key : key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home : Homepage()
+    );
+  }
 }
 
